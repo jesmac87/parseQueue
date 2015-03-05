ed05a34bb90e6ebe1a985b3754c4a77219b4d897
Jesse Mace <jesmac87@gmail.com>
2015-03-04 21:16:14 -0700

    Addn question layout and toggle status capability

--- js/MainController.js
+++ js/MainController.js
@@ -5,9 +5,32 @@ app.controller('MainController', function($scope, parseService) {
 
     $scope.postData = function() {
         parseService.POST($scope.questionText).then(function(response) {
-            console.log(response.data);
+            $scope.getData();
         });
 
-        // $scope.questionText(); // reset question input text box
+        $scope.questionText = ''; // reset question input text box
+    };
+
+    $scope.getData = function() {
+        parseService.getParseData().then(function(response) {
+            $scope.questions = response.data.results;
+            console.log($scope.questions);
+        });
+    };
+
+    $scope.getData(); //initial load of question data
+
+    $scope.updateData = function(objectId, status) {
+
+        console.log(status);
+        if (status === 'Red') {
+            status = 'Yellow';
+        } else if (status === 'Yellow') {
+            status = 'Red';
+        }
+
+        parseService.updateParseData(objectId, status).then(function(response) {
+            $scope.getData();
+        });
     };
 });
--- js/parseService.js
+++ js/parseService.js
@@ -2,16 +2,20 @@ var app = angular.module('parseQ');
 
 app.service('parseService', function($http) {
 
-
-
     this.POST = function(question) {
         return $http.post('https://api.parse.com/1/classes/Questions', {
-            'question': question
+            'text': question,
+            'status': 'Red',
         });
-
-
     };
 
+    this.getParseData = function() {
+        return $http.get('https://api.parse.com/1/classes/Questions');
+    };
 
-
+    this.updateParseData = function(objectId, status) {
+        return $http.put('https://api.parse.com/1/classes/Questions/' + objectId, {
+            'status': status,
+        });
+    };
 });
